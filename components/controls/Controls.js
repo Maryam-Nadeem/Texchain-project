9404543564a72a734d6e1af1618aa668ce108105
Maryam-Nadeem <maryamnadeem821@gmail.com>
2021-09-24 00:33:08 +0500

    commit after deleting library radio group

--- components/controls/Controls.js
+++ components/controls/Controls.js
@@ -1,14 +1,11 @@
 import Input from "./Input";
-import RadioGroup from "./RadioGroup";
 import Select from "./Select";
 import Checkbox from "./Checkbox";
-//import DatePicker from "./DatePicker";
 import MainButton from "./Button";
 import ActionButton from './ActionButton';
 
 const Controls = {
     Input,
-    RadioGroup,
     Select,
     Checkbox,
     MainButton,
